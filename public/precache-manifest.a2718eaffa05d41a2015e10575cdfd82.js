71f38a272bfffe9698444d3723d5b8dd1ae29836
Edwin <Edwin.Martinez@people10.com>
2020-03-30 21:57:01 -0400

    added Launch

---
+++ public/precache-manifest.a2718eaffa05d41a2015e10575cdfd82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a191dafb3b5ec343861d9d13cb36f233",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba2683dfad921e8aa13",
+    "url": "/static/css/main.d1c82d0d.chunk.css"
+  },
+  {
+    "revision": "3de33a23885240183692",
+    "url": "/static/js/2.6959aff3.chunk.js"
+  },
+  {
+    "revision": "f0d43bb23094b147d2c500466b241ef2",
+    "url": "/static/js/2.6959aff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba2683dfad921e8aa13",
+    "url": "/static/js/main.aee79893.chunk.js"
+  },
+  {
+    "revision": "4730c68c8def48fa1909",
+    "url": "/static/js/runtime-main.687c1938.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
+
+//Allow cross-origin
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   graphiql: true
 }));
+
+//set static folder
+app.use(express.static('public'));
+//when ever any route is hit, it will redirect to 
+//react's index.html page in the public folder
+//the folder afte we build
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public","index.html"));
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
